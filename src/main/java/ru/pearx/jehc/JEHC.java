86de5f31cee5a25451a74aa928e3ce0713480bd4
mrAppleXZ <mr.applexz@gmail.com>
2017-07-19 13:37:15 +0500

    Bump

--- src/main/java/ru/pearx/jehc/JEHC.java
+++ src/main/java/ru/pearx/jehc/JEHC.java
@@ -17,7 +17,7 @@ public class JEHC
 {
     public static final String NAME = "Just Enough HarvestCraft";
     public static final String MODID = "jehc";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.3.0";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e)
